b8eb88b8ade1d01d4a588b7ee9928557a991d253
devillove084 <786537003@qq.com>
2020-10-13 11:32:03 +0800

    pmem_2

--- nvm_engine/PersistantPool.hpp
+++ nvm_engine/PersistantPool.hpp
@@ -28,9 +28,9 @@ struct KVSRef {
 
 enum ValueEncoding {
     kEncodingRawCompressed = 0x0,
-    kEncodingRawUnCompressed = 0x01,
+    kEncodingRawUncompressed = 0x01,
     kEncodingPtrCompressed = 0x02,
-    kEncodingPtrUnCompressed = 0x03,
+    kEncodingPtrUncompressed = 0x03,
     kEncodingUnknown
 };
 
@@ -59,6 +59,7 @@ struct KVSRoot {
     size_t size;
 };
 
+
 static struct Pool* pools_ = nullptr;
 static size_t pool_count_;
 static std::atomic<size_t> next_pool_index_(0);
@@ -69,6 +70,45 @@ static size_t dcpmm_avail_size_min_ = 0;
 
 static std::atomic<size_t> dcpmm_avail_size_(0);
 static std::atomic<bool> dcpmm_is_avail_(true);
+
+inline bool Snappy_Compress(const char* input,
+                            size_t length, ::std::string* output) {
+#ifdef SNAPPY
+  output->resize(snappy::MaxCompressedLength(length));
+  size_t outlen;
+  snappy::RawCompress(input, length, &(*output)[0], &outlen);
+  output->resize(outlen);
+  return true;
+#else
+  (void)input;
+  (void)length;
+  (void)output;
+  return false;
+#endif
+}
+
+inline bool Snappy_GetUncompressedLength(const char* input, size_t length,
+                                         size_t* result) {
+#ifdef SNAPPY
+  return snappy::GetUncompressedLength(input, length, result);
+#else
+  (void)input;
+  (void)length;
+  (void)result;
+  return false;
+#endif
+}
+
+inline bool Snappy_Uncompress(const char* input, size_t length, char* output) {
+#ifdef SNAPPY
+  return snappy::RawUncompress(input, length, output);
+#else
+  (void)input;
+  (void)length;
+  (void)output;
+  return false;
+#endif
+}
 
 int KVSOpen(const char* path, size_t size, size_t pool_count) {
     assert(!pools_);
@@ -201,3 +241,145 @@ inline static bool KVSEncodeValue(const Slice& value, bool compress, KVSRef* ref
     return true;
 }
 
+inline static void FreePmem(struct KVSRef* ref) {
+    PMEMoid oid;
+    oid.pool_uuid_lo = pools_[ref->pool_index].uuid_lo;
+    oid.off = ref->off_in_pool;
+    pmemobj_free(&oid);
+    if (!dcpmm_is_avail_) {
+        if ((dcpmm_avail_size_ += ref->size) > dcpmm_avail_size_min_) {
+        dcpmm_avail_size_ = 0;
+        dcpmm_is_avail_ = true;
+        }
+    }
+}
+
+void KVSDumpFromValueRef(const char* input,
+                           std::function<void(const Slice& value)> add) {
+    assert(pools_);
+    auto* ref = (struct KVSRef*)input;
+    if (ref->hdr.encoding == kEncodingPtrCompressed ||
+        ref->hdr.encoding == kEncodingPtrUncompressed) {
+        auto* hdr = (struct KVSHdr*)(pools_[ref->pool_index].base_addr
+                                        + ref->off_in_pool);
+        if (ref->hdr.encoding == kEncodingPtrCompressed) {
+        hdr->encoding = kEncodingRawCompressed;
+        }
+        else {
+        hdr->encoding = kEncodingRawUncompressed;
+        }
+
+        // Prefix encoding type of the value content.
+        add(Slice((char*)hdr, ref->size + sizeof(struct KVSHdr)));
+
+        FreePmem(ref);
+    }
+}
+
+void KVSDecodeValueRef(const char* input, size_t size, std::string* dst) {
+    assert(input);
+    auto encoding = KVSGetEncoding(input);
+    const char* src_data;
+    size_t src_len;
+
+    // if it is indirectly pointed, input is a KVSRef
+    if (encoding == kEncodingPtrUncompressed ||
+            encoding == kEncodingPtrCompressed) {
+        assert(size == sizeof(struct KVSRef));
+        auto* ref = (struct KVSRef*)input;
+        // the data on DCPMM
+        src_data = (char*)pools_[ref->pool_index].base_addr +
+                    ref->off_in_pool + sizeof(struct KVSHdr);
+        src_len = ref->size;
+    }
+    else {
+        assert(encoding == kEncodingRawUncompressed ||
+                encoding == kEncodingRawCompressed);
+        assert(size >= sizeof(struct KVSHdr));
+        src_data = input + sizeof(struct KVSHdr);
+        src_len = size - sizeof(struct KVSHdr);
+    }
+
+    // if not compressed
+    if (encoding == kEncodingRawUncompressed ||
+        encoding == kEncodingPtrUncompressed) {
+        dst->assign(src_data, src_len);
+    }
+    // else need to decompress
+    else
+    {
+        assert(encoding == kEncodingRawCompressed ||
+        encoding == kEncodingPtrCompressed);
+        size_t dst_len;
+        if (Snappy_GetUncompressedLength(src_data, src_len, &dst_len)) {
+        char* tmp_buf = new char[dst_len];
+        Snappy_Uncompress(src_data, src_len, tmp_buf);
+        dst->assign(tmp_buf, dst_len);
+        delete tmp_buf;
+        } else {
+        abort();
+        }
+    }
+}
+
+size_t KVSGetExtraValueSize(const Slice& value) {
+    auto* ref = (struct KVSRef*)value.data();
+    if (ref->hdr.encoding == kEncodingRawCompressed ||
+        ref->hdr.encoding == kEncodingRawUncompressed) {
+        return 0;
+    }
+    else {
+        return (size_t)ref->size;
+    }
+}
+
+void KVSFreeValue(const Slice& value) {
+    auto* ref = (struct KVSRef*)value.data();
+    if (ref && ref->hdr.encoding != kEncodingRawCompressed &&
+        ref->hdr.encoding != kEncodingRawUncompressed) {
+        FreePmem(ref);
+    }
+}
+
+void KVSSetKVSValueThres(size_t thres) {
+    kvs_value_thres_ = thres;
+}
+
+size_t KVSGetKVSValueThres() {
+    return kvs_value_thres_;
+}
+
+void KVSSetCompressKnob(bool compress) {
+    compress_value_ = compress;
+}
+
+bool KVSGetCompressKnob() {
+    return compress_value_;
+}
+
+int KVSPublish(struct pobj_action** pact_array, size_t actvcnt) {
+    assert(pools_);
+    auto* pacts_of_pools = new std::vector<struct pobj_action>[pool_count_];
+    for (size_t i = 0; i < actvcnt; i++) {
+        auto* pact = (struct PobjAction*)pact_array[i];
+        assert(pact->pool_index < pool_count_);
+        pacts_of_pools[pact->pool_index].push_back(*((struct pobj_action*)pact));
+        delete pact;
+    }
+    for (size_t i = 0; i < pool_count_; i++) {
+        auto* pool = pools_[i].pool;
+        auto& pacts = pacts_of_pools[i];
+        auto* data = pacts.data();
+        size_t count = pacts.size();
+        size_t index = 0;
+        while(index < count) {
+        auto n = std::min(count - index, 32UL);
+        pmemobj_publish(pool, data + index, n);
+        index += n;
+        }
+        assert(index == count);
+    }
+    delete[] pacts_of_pools;
+
+    return 0;
+}
